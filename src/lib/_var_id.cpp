8acc1d2607d8465e907963a62eeac659990292c6
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-06-23 18:56:31 +0200

    Revert "use boost::atomic_uint for new_variable"
    This reverts commit 170383b913afeb773d7833823d41450147e119bf.

--- src/lib/_var_id.cpp
+++ src/lib/_var_id.cpp
@@ -1,12 +1,10 @@
 #include "../metaSMT/impl/_var_id.hpp"
 
-#include <boost/atomic.hpp>
-
 namespace metaSMT {
   namespace impl {
       unsigned new_var_id(  )
       {
-        static boost::atomic_uint _id ( 0u );
+        static unsigned _id = 0;
         ++_id;
         return _id;
       } 
