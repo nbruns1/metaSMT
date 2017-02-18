580fec187f6121d336a9e3e8d16e6f15041063f8
Hoang <hle@cs.uni-bremen.de>
2017-02-18 15:18:41 +0100

    replace boost::atomic with std::atomic

--- src/lib/_var_id.cpp
+++ src/lib/_var_id.cpp
@@ -1,24 +1,11 @@
 #include "../metaSMT/impl/_var_id.hpp"
-#include <boost/version.hpp>
 
-#if BOOST_VERSION >= 105500
-
-#include <boost/atomic.hpp>
-unsigned metaSMT::impl::new_var_id()
-{
-  static boost::atomic_uint _id ( 0u );
-  ++_id;
-  return _id;
-} 
-
-#else
+#include <atomic>
 
 unsigned metaSMT::impl::new_var_id()
 {
-  static unsigned _id ( 0u );
+  static std::atomic_uint _id ( 0u );
   ++_id;
   return _id;
 } 
-
-#endif
 
