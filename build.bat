46427fdcc896990abf7b02fb3f32dfbd237c1b59
Niklas Bruns <nbruns@uni-bremen.de>
2017-03-28 11:34:20 +0200

    fix

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake .. -DBOOST_ROOT="C:\projects\deps\boost" -DZ3_DIR="C:\projects\deps\z3-4.5.0-x86-win"
+cmake .. -DBoost_USE_STATIC_LIBS=ON -DBOOST_ROOT="C:\projects\deps\boost" -DZ3_DIR="C:\projects\deps\z3-4.5.0-x86-win"
 cat CMakeFiles/CMakeError.log
