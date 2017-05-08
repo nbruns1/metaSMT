a73405909e6c4c12433046e3b17cf586183c895b
Niklas Bruns <nbruns@uni-bremen.de>
2017-05-08 14:30:25 +0200

    fix DirectSolver_Context.hpp(70): warning C4244: 'initializing': conversion from '__int64' to 'long', possible loss of data

--- src/metaSMT/DirectSolver_Context.hpp
+++ src/metaSMT/DirectSolver_Context.hpp
@@ -67,8 +67,8 @@ namespace metaSMT {
         , Expr1 value
         , Expr2 bw
     ) {
-      const long val            = proto::value(value);
-      const unsigned long width = proto::value(bw);
+      const int64_t val            = proto::value(value);
+      const uint64_t width = proto::value(bw);
 
       return  SolverContext::operator() ( tag, 
         boost::any(boost::make_tuple(val, width))
