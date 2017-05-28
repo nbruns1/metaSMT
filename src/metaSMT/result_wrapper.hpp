4889c6d40f397bd2856b401aa3e17a5afdb607fe
Niklas Bruns <nbruns@uni-bremen.de>
2017-05-28 20:09:12 +0200

    split for fix 311

--- src/metaSMT/result_wrapper.hpp
+++ src/metaSMT/result_wrapper.hpp
@@ -308,7 +308,7 @@ namespace metaSMT {
           std::cout << "isSigned: " << isSigned << std::endl;
           if( isSigned ) ret = static_cast<Integer>(-1) ;
           for (unsigned i = 0; i < val.size(); ++i) {
-            bool isSignedTmp = isSigned ? static_cast<bool>(1) : static_cast<bool>(0));
+            bool isSignedTmp = isSigned ? static_cast<bool>(1) : static_cast<bool>(0);
             ret ^= Integer( val[i]^isSignedTmp) << i;
           }
           std::cout << "ret: " << ret << std::endl;
