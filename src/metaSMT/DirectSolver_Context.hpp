06e151510294bccaae7dc859e3657c82f8df8cbd
Niklas Bruns <nbruns@uni-bremen.de>
2017-06-01 08:42:49 +0200

    add debug output to bvuint_tag and bvsint_tag

--- src/metaSMT/DirectSolver_Context.hpp
+++ src/metaSMT/DirectSolver_Context.hpp
@@ -55,6 +55,7 @@ namespace metaSMT {
     ) {
       const uint64_t val   = proto::value(value);
       const unsigned width = proto::value(bw);
+      std::cout << "bvuint_tag:val: " << val << std::endl;
 
       return SolverContext::operator() ( tag,
         boost::any(boost::make_tuple(val, width))
@@ -69,6 +70,7 @@ namespace metaSMT {
     ) {
       const int64_t val            = proto::value(value);
       const unsigned width = proto::value(bw);
+      std::cout << "buint_tag:val: " << val << std::endl;
 
       return  SolverContext::operator() ( tag, 
         boost::any(boost::make_tuple(val, width))
